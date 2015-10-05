d96675d4fb092f966817c20915a6a32efd75251d
Julio <juliofornode@gmail.com>
2015-10-05 08:46:43 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## MEANjs Google Maps CRUD
+
+A simple CRUD for Google Map markers based on the example Article CRUD module in the MEANjs generator.
---
+++ public/modules/articles/config/articles.client.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Configuring the Articles module
+angular.module('articles').run(['Menus',
+	function(Menus) {
+		// Set top bar menu items
+		Menus.addMenuItem('topbar', 'Events', 'articles', 'dropdown', '/articles(/create)?');
+		Menus.addSubMenuItem('topbar', 'articles', 'List Events', 'articles');
+		Menus.addSubMenuItem('topbar', 'articles', 'New Event', 'articles/create');
+	}
+]);
